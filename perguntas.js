ee959b3be6ee80e5de8435e84d776fc8a663d67b
Arthur Silva <168222534+thurinkz@users.noreply.github.com>
2024-11-28 17:24:36 -0300

    sagui

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação usada principalmente para criar interatividade em páginas web.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    'O maior país do mundo em extensão territorial é a Rússia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de passos para resolver um problema ou realizar uma tarefa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz aprender em Inglês?',
+    'Aprender em inglês é learn.'
+)
